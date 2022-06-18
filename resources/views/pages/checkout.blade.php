7f1be4da1f34901ad0cd7f5ee667c48f98310287
Ferdy <ferdyarya5@gmail.com>
2022-06-18 14:33:54 +0800

    update checkout

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -1,181 +1,216 @@
 @extends('layouts.checkout')
-@section('title', 'Detail Travel')
+@section('title')
+Checkout
+@endsection
 
 @section('content')
 <main>
-    <section class="section-details-header"></section>
-    <section class="section-details-content">
-        <div class="container">
-            <div class="row">
-                <div class="col p-0">
-                    <nav>
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item">Paket Travel</li>
-                            <li class="breadcrumb-item ">Details</li>
-                            <li class="breadcrumb-item active">Checkout</li>
-                        </ol>
-                    </nav>
-                </div>
-            </div>
+<section class="section-details-header"></section>
+<section class="section-details-content">
+<div class="container">
+    <div class="row">
+        <div class="col p-0">
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">Paket Travel</li>
+                    <li class="breadcrumb-item ">Details</li>
+                    <li class="breadcrumb-item active">Checkout</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
 
-            <div class="row">
-                <div class="col-lg-8 pl-lg-0">
-                    <div class="card card-details">
-                        <h1>Who is Going?</h1>
-                        <p>Trip to Ubud, Bali, Indonesia</p>
-                        <div class="attendee">
-                            <table class="table table-responsive-sm text-center">
-                                <thead>
-                                    <tr>
-                                        <td>Picture</td>
-                                        <td>Name</td>
-                                        <td>Nationality</td>
-                                        <td>VISA</td>
-                                        <td>Passport</td>
-                                        <td></td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td><img src="{{ url('frontend/images/avatar1.png')}}" height="60px"></td>
-                                        <td class="align-middle">Angga Rizky</td>
-                                        <td class="align-middle">CN</td>
-                                        <td class="align-middle">N/A</td>
-                                        <td class="align-middle">Active</td>
-                                        <td class="align-middle"> <a href=""><img src="frontend/images/remove.png" alt=""></a></td>
-                                    </tr>
-
-                                    <tr>
-                                        <td><img src="{{ url('frontend/images/avatar2.png')}}" height="60px"></td>
-                                        <td class="align-middle">Galih Pratama</td>
-                                        <td class="align-middle">SG</td>
-                                        <td class="align-middle">30 Days</td>
-                                        <td class="align-middle">Active</td>
-                                        <td class="align-middle"> <a href=""><img src="{{ url('frontend/images/remove.png')}}" alt=""></a></td>
-                                    </tr>                                            
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="member mt-3">
-                            <h2>Add Member</h2>
-                            <form class="form-inline">
-                                <label for="inputUsername" class="sr-only">Name</label>
-                                <input type="text" name="inputUsername" class="form-control mb-2 mr-sm-2" 
-                                id="inputUsername" placeholder="Username">
-
-                                <label for="inputVisa" class="sr-only">VISA</label>
-                                <select name="inputVisa" id="inputVisa" class="custom-select mb-2 mr-sm-2">
-                                    <option value="VISA">VISA</option>
-                                    <option value="30 Days">30 Days</option>
-                                    <option value="N/A">N/A</option>
-                                </select>
-
-                            <label for="doePassword" class="sr-only">DOE Passport</label>                                        
-                                <div class="input-group mb-2 mr-sm-2">
-                                    <input type="text" class="form-control datepicker" 
-                                    id="doePassport" placeholder="DOE Passport">
-                                </div>
-                            
-                            <button type="submit" class="btn btn-add-now mb-2 px-4">Add Now</button>
-                            </form>
-
-                            <h3 class="mt-2 mb-0">Note</h3>
-                            <p class="disclaimer mb-0">
-                                You are only able to invite member that has registered in Nomads.
-                            </p>
-                        </div>
+    <div class="row">
+        <div class="col-lg-8 pl-lg-0">
+            <div class="card card-details">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error )
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
                     </div>
-                </div>
-            <div class="col-lg-4">
-                <div class="card card-details card-right">                                
-                   
-                    <h2>Checkout Information</h2>
-                    <table class="trip-information">
-                        <tr>
-                            <th width="50%">Members</th>
-                            <td width="50%" class="text-right">2 Person</td>
-                        </tr>
-                        <tr>
-                            <th width="50%">Additional VISA</th>
-                            <td width="50%" class="text-right">$ 190,00</td>
-                        </tr>
-                        <tr>
-                            <th width="50%">Trip Price</th>
-                            <td width="50%" class="text-right">$ 80,00 / person</td>
-                        </tr>
-                        <tr>
-                            <th width="50%">Sub Total</th>
-                            <td width="50%" class="text-right">$ 280,00</td>
-                        </tr>
-                        <tr>
-                            <th width="50%">Total (+Unique Code)</th>
-                            <td width="50%" class="text-right text-total">
-                                <span class="text-blue">$ 279,</span>
-                                <span class="text-orange">33</span>
-                            </td>
-                        </tr>
+                @endif
+                <h1>Who is Going?</h1>
+                <p>Trip to {{ $item->travel_package->title }}, {{ $item->travel_package->location }}</p>
+                <div class="attendee">
+                    <table class="table table-responsive-sm text-center">
+                        <thead>
+                            <tr>
+                                <td>Picture</td>
+                                <td>Name</td>
+                                <td>Nationality</td>
+                                <td>VISA</td>
+                                <td>Passport</td>
+                                <td></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($item->details as $detail)
+                            <tr>
+                                <td>
+                                    <img src="https://ui-avatars.com/api/?name={{ $detail->username }}" height="60px" class="rounded-circle">
+                                </td>
+
+                                <td class="align-middle">
+                                    {{ $detail->username }}
+                                </td>
+                                
+                                <td class="align-middle">
+                                    {{ $detail->nationality }}
+                                </td>
+                                
+                                <td class="align-middle">
+                                    {{ $detail->is_visa ?  '30 Days' : 'N/A' }}
+                                </td>
+                                
+                                <td class="align-middle">
+                                    {{ \Carbon\Carbon::createFromDate($detail->doe_password) > \Carbon\Carbon::now() ? 'Active' : 'Inactive' }}
+                                </td>
+
+                                <td class="align-middle">
+                                     <a href="{{ route('checkout-remove', $detail->id) }}">
+                                <img src="{{ url('frontend/images/remove.png') }}" alt=""></a>
+                                </td>
+                            </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="6" class="text-center">No Visitor</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
                     </table>
-                    <hr>
-                    <h2>Payment Instructions</h2>
-                    <p class="payment-instruction">
-                        Please complete your payment before to continue the wonderful trip
-                    </p>
-                    <div class="bank">
-                        <div class="bank-item pb-3">
-                            <img src="{{ url('frontend/images/ic_bank.jpg')}}" alt="" class="bank-image">
-                            <div class="description">
-                                <h3>PT Nomads ID</h3>
-                                <p>
-                                0819 4334 7283
-                                <br>
-                                Bank Central Asia
-                                </p>
-                            </div>
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="bank-item pb-3">
-                            <img src="{{ url('frontend/images/ic_bank.jpg')}}" alt="" class="bank-image">
-                            <div class="description">
-                                <h3>PT Nomads ID</h3>
-                                <p>
-                                0899 4334 7283
-                                <br>
-                                Bank HSBC   
-                                </p>
-                            </div>
-                            <div class="clearfix">
-                            </div>
-                        </div>
+                </div>
+                <div class="member mt-3">
+                    <h2>Add Member</h2>
+                    <form class="form-inline" method="POST" action="{{ route('checkout-create', $item->id) }}">
+                        @csrf
+                        <label for="username" class="sr-only">Name</label>
+                        <input type="text" name="username" class="form-control mb-2 mr-sm-2" 
+                        id="username" required placeholder="Username">
+
+                        <label for="nationality" class="sr-only">Nationality</label>
+                        <input type="text" name="nationality" class="form-control mb-2 mr-sm-2" style="width: 50px"
+                        id="nationality" required placeholder="Nationality">
+
+
+                        <label for="is_visa" class="sr-only">VISA</label>
+                        <select name="is_visa" id="is_visa" required class="custom-select mb-2 mr-sm-2">
+                            <option value="" selected>VISA</option>
+                            <option value="1">30 Days</option>
+                            <option value="0">N/A</option>
+                        </select>
+
+                    <label for="doe_password" class="sr-only">DOE Passport</label>
+                    <div class="input-group mb-2 mr-sm-2">
+                        <input type="text" class="form-control datepicker" name="doe_password" 
+                        id="doepassword" placeholder="DOE Passport">
+
                     </div>
+                    <button type="submit" class="btn btn-add-now mb-2 px-3">Add Now</button>
+                    
+                    
+                    </form>
+
+                    <h3 class="mt-2 mb-0">Note</h3>
+                    <p class="disclaimer mb-0">
+                        You are only able to invite member that has registered in Nomads.
+                    </p>
                 </div>
-                <div class="join-container">
-                    <a href="{{ route('checkout-success')}}" class="btn btn-block btn-join-now mt-3 py-2">I Have Made Payment</a>
-                </div>
-                <div class="text-center mt-3">
-                    <a href="{{ url('/detail')}}" class="text-muted">Cancel Booking</a>
-                </div>
-            </div>
             </div>
         </div>
-    </section>
+    <div class="col-lg-4">
+        <div class="card card-details card-right">                                
+            
+            <h2>Checkout Information</h2>
+            <table class="trip-information">
+                <tr>
+                    <th width="50%">Members</th>
+                    <td width="50%" class="text-right">{{ $item->details->count() }} Person</td>
+                </tr>
+                <tr>
+                    <th width="50%">Additional VISA</th>
+                    <td width="50%" class="text-right">$ {{ $item->additional_visa }},00</td>
+                </tr>
+                <tr>
+                    <th width="50%">Trip Price</th>
+                    <td width="50%" class="text-right">$ {{ $item->travel_package->price }},00 / person</td>
+                </tr>
+                <tr>
+                    <th width="50%">Sub Total</th>
+                    <td width="50%" class="text-right">$ {{ $item->transaction_total }},00</td>
+                </tr>
+                <tr>
+                    <th width="50%">Total (+Unique Code)</th>
+                    <td width="50%" class="text-right text-total">
+                        <span class="text-blue">$ {{ $item->transaction_total }}</span>
+                        <span class="text-orange">{{ mt_rand(0,99) }}</span>
+                    </td>
+                </tr>
+            </table>
+            <hr>
+            <h2>Payment Instructions</h2>
+            <p class="payment-instruction">
+                Please complete your payment before to continue the wonderful trip
+            </p>
+            <div class="bank">
+                <div class="bank-item pb-3">
+                    <img src="{{ url('frontend/images/ic_bank.jpg') }}" alt="" class="bank-image">
+                    <div class="description">
+                        <h3>PT Nomads ID</h3>
+                        <p>
+                        0819 4334 7283
+                        <br>
+                        Bank Central Asia
+                        </p>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="bank-item pb-3">
+                    <img src="{{ url('frontend/images/ic_bank.jpg') }}" alt="" class="bank-image">
+                    <div class="description">
+                        <h3>PT Nomads ID</h3>
+                        <p>
+                        0899 4334 7283
+                        <br>
+                        Bank HSBC   
+                        </p>
+                    </div>
+                    <div class="clearfix">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="join-container">
+            <a href="{{ route('checkout-success', $item->id) }}" class="btn btn-block btn-join-now mt-3 py-2">I Have Made Payment</a>
+        </div>
+        <div class="text-center mt-3">
+            <a href="{{ route('details', $item->travel_package->slug) }}" class="text-muted">Cancel Booking</a>
+        </div>
+    </div>
+    </div>
+</div>
+</section>
 </main>
 @endsection
 
 @push('prepend-style')
-<link rel="stylesheet" href="{{ url('frontend/libraries/gijgo/css/gijgo.min.css')}}">
+<link rel="stylesheet" href="{{ url('frontend/libraries/gijgo/css/gijgo.min.css') }}">
 @endpush
 
-@push('addon-script')
-<script src="{{ url('frontend/libraries/gijgo/js/gijgo.min.js')}}"></script>
+@push('prepend-script')
+<script src="{{ url('frontend/libraries/gijgo/js/gijgo.min.js') }}"></script>
+
 <script>
-    $(document).ready(function() {
-        
-        $('.datepicker').datepicker({
-            uiLibrary: 'bootstrap4',
-            icons: {
-                rightIcon: '<img src="{{ url('frontend/images/ic_doe.png')}}">'
-            }
-        })
-    });
+$(document).ready(function() {
+$('.datepicker').datepicker({
+    format: 'yyyy-mm-dd',
+    uiLibrary: 'bootstrap4',
+    icons: {
+        rightIcon: '<img src="{{ url('frontend/images/ic_doe.png') }}">'
+    }
+})
+});
 </script>
 @endpush
